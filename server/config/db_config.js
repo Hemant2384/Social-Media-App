f403c6705199a3106ca6c2bcb189df12bca61c0a
Hemant Jain <60097403+Hemant2384@users.noreply.github.com>
2022-06-17 00:16:18 +0530

    Update db_config.js

--- server/config/db_config.js
+++ server/config/db_config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require("dotenv").config({ path: '../.env' });
 const connectDB = async() => {
     try{
-        mongoose.connect("mongodb+srv://mongotut:testing123@cluster0.olqwa.mongodb.net/GraphqlAPI?retryWrites=true&w=majority",{
+        mongoose.connect("Your mongo connect url",{
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
